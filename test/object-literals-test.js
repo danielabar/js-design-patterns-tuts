4124a0ba23b50f7602207ad10162ae54290b1cb9
Daniela Baron <baron.daniela@gmail.com>
2014-12-05 15:48:40 -0500

    more tests for object properties

--- test/object-literals-test.js
+++ test/object-literals-test.js
@@ -6,8 +6,26 @@ exports.testDotNotation = function(test) {
   test.done();
 };
 
-exports.squareBracketsNotation = function(test) {
+exports.testSquareBracketsNotation = function(test) {
   test.expect(1);
   test.equal(ol.getObject()['lastName'], 'Smith', 'property is accessible via square bracket notation');
+  test.done();
+};
+
+exports.testKeyWithSpaces = function(test) {
+  test.expect(1);
+  test.equal(ol.getObject()['key with spaces is ok'], 'this key has spaces and its ok', 'square bracket notation allows keys with spaces');
+  test.done();
+};
+
+exports.testKeyWithReservedKeyword = function(test) {
+  test.expect(1);
+  test.equal(ol.getObject()['new'], 'new is a reserved keyword', 'square bracket notation allows keys with reserved keywords');
+  test.done();
+};
+
+exports.testDefineProperty = function(test) {
+  test.expect(1);
+  test.equal(ol.getObject()['country'], 'Canada', 'properies can be set via ES5 defineProperty');
   test.done();
 };
